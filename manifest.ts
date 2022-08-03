5e6bf5d4c31e41885bfa07aad0de51be218ade04
Brad Harris <bharris@slack-corp.com>
2022-08-03 14:46:24 -0600

    bumping sdk to 0.1.0

--- manifest.ts
+++ manifest.ts
@@ -1,10 +1,16 @@
-import { DefineFunction, Manifest, Schema } from "deno-slack-sdk/mod.ts";
+import {
+  DefineFunction,
+  DefineWorkflow,
+  Manifest,
+  Schema,
+} from "deno-slack-sdk/mod.ts";
 
 export default Manifest({
   name: "starter-template",
   description: "A starter template.",
   icon: "assets/icon.png",
   functions: [],
+  workflows: [],
   outgoingDomains: [],
   botScopes: ["commands", "chat:write", "chat:write.public"],
 });
