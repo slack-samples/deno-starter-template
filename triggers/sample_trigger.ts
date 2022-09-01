6ac30945143368db4a97e61a8bbabc7404f63cb6
Neil Shapiro <nshapiro@slack-corp.com>
2022-09-01 10:33:26 -0700

    Update SDK and API versions

--- triggers/sample_trigger.ts
+++ triggers/sample_trigger.ts
@@ -1,12 +1,12 @@
 import { Trigger } from "deno-slack-api/types.ts";
-
+import SampleWorkflow from "../workflows/sample_workflow.ts";
 /**
  * Triggers determine when Workflows are executed. A trigger
  * file describes a scenario in which a workflow should be run,
  * such as a user pressing a button or when a specific event occurs.
  * https://api.slack.com/future/triggers
  */
-const sampleTrigger: Trigger = {
+const sampleTrigger: Trigger<typeof SampleWorkflow.definition> = {
   type: "shortcut",
   name: "Sample trigger",
   description: "A sample trigger",
