e17901ee95ac98d768401c1bc2feddba9fc92fde
Horea Porutiu <hporutiu@slack-corp.com>
2022-09-30 10:56:57 -0400

    changed workflow to use --allow-net to pass test

--- functions/sample_function.ts
+++ functions/sample_function.ts
@@ -36,22 +36,27 @@ export const SampleFunctionDefinition = DefineFunction({
   },
 });
 
+// This function takes the input from the open form step, adds some formatting
+// and returns the updated message. The updated message is the input to a
+// sendMessage call, which posts this updated message into the channel the user selected.
+// This function also calls the Slack hosted datastore to save our object to
+// our datastore once we update it with details such as an object id.
 export default SlackFunction(
   SampleFunctionDefinition,
-  ({ inputs, token }) => {
+  async ({ inputs, token }) => {
     const sampleObject = {} as Record<string, unknown>;
     sampleObject.original_msg = inputs.message;
 
-    //grab the current user via interactivity, and output that in the
+    //grab the current user via interactivity, and output that in the message
     const updatedMsg = `:wave: ` + `<@${inputs.user}>` +
       ` submitted the following message: \n\n>${inputs.message}`;
     sampleObject.updated_msg = updatedMsg;
     const client = SlackAPI(token, {});
     sampleObject.object_id = "OBJ-" +
-      (Math.floor(100000 + Math.random() * 900000));
+      await (Math.floor(100000 + Math.random() * 900000));
 
     //save the sample Object
-    client.apps.datastore.put(
+    await client.apps.datastore.put(
       {
         datastore: "SampleObjects",
         item: sampleObject,
--- functions/sample_function_test.ts
+++ functions/sample_function_test.ts
@@ -2,30 +2,14 @@ import { SlackFunctionTester } from "deno-slack-sdk/mod.ts";
 import { assertEquals } from "https://deno.land/std@0.153.0/testing/asserts.ts";
 import SampleFunction from "./sample_function.ts";
 // import { SlackAPI } from "deno-slack-api/mod.ts";
-import * as mf from "https://deno.land/x/mock_fetch@0.3.0/mod.ts";
-
-// Replaces globalThis.fetch with the mocked copy
-mf.install();
-
-mf.mock("POST@/api/auth.test", async (req: any) => {
-  const body = await req.formData();
-  console.log(body);
-  return body;
-});
-
-mf.mock("POST@/api/apps.datastore.put", async (req: any) => {
-  const body = await req.formData();
-  console.log(body);
-  return body;
-});
 
 const { createContext } = SlackFunctionTester("sample_function");
 
 Deno.test("Sample function test", async () => {
-  const inputs = { message: "Hello, World!" };
+  const inputs = { message: "Hello, World!", user: "U01234567" };
   const { outputs } = await SampleFunction(createContext({ inputs }));
-  assertEquals(
+  await assertEquals(
     outputs?.updatedMsg,
-    ":wave: You submitted the following message: \n\n>Hello, World!",
+    ":wave: <@U01234567> submitted the following message: \n\n>Hello, World!",
   );
 });
