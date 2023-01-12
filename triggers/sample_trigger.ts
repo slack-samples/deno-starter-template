0800b3ab6b3311f710761502b5f115702af6d7f0
Ashley Huynh <ashley.huynh@slack-corp.com>
2023-01-12 12:21:27 -0500

    Update capitalization for next-gen terms

--- triggers/sample_trigger.ts
+++ triggers/sample_trigger.ts
@@ -1,7 +1,7 @@
 import { Trigger } from "deno-slack-api/types.ts";
 import SampleWorkflow from "../workflows/sample_workflow.ts";
 /**
- * Triggers determine when Workflows are executed. A trigger
+ * Triggers determine when workflows are executed. A trigger
  * file describes a scenario in which a workflow should be run,
  * such as a user pressing a button or when a specific event occurs.
  * https://api.slack.com/future/triggers
