f3a1b40e716d42293ab54af362d021916dac60f1
Filip Maj <fmaj@slack-corp.com>
2023-05-09 10:41:58 -0400

    Use trigger data and type enumerations in trigger definition. Dont hardcode workflow callback id in trigger definition.

--- triggers/sample_trigger.ts
+++ triggers/sample_trigger.ts
@@ -1,4 +1,5 @@
 import { Trigger } from "deno-slack-sdk/types.ts";
+import { TriggerContextData, TriggerTypes } from "deno-slack-api/mod.ts";
 import SampleWorkflow from "../workflows/sample_workflow.ts";
 /**
  * Triggers determine when workflows are executed. A trigger
@@ -7,19 +8,19 @@ import SampleWorkflow from "../workflows/sample_workflow.ts";
  * https://api.slack.com/automation/triggers
  */
 const sampleTrigger: Trigger<typeof SampleWorkflow.definition> = {
-  type: "shortcut",
+  type: TriggerTypes.Shortcut,
   name: "Sample trigger",
   description: "A sample trigger",
-  workflow: "#/workflows/sample_workflow",
+  workflow: `#/workflows/${SampleWorkflow.definition.callback_id}`,
   inputs: {
     interactivity: {
-      value: "{{data.interactivity}}",
+      value: TriggerContextData.Shortcut.interactivity,
     },
     channel: {
-      value: "{{data.channel_id}}",
+      value: TriggerContextData.Shortcut.channel_id,
     },
     user: {
-      value: "{{data.user_id}}",
+      value: TriggerContextData.Shortcut.user_id,
     },
   },
 };
