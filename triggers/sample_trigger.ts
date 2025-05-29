f3383ae850bca9ce634334bcc02c50362e9d8557
William Bergamin <wbergamin@salesforce.com>
2025-05-29 10:46:02 -0400

    chore: update to latests Deno and CLI standards (#77)

--- triggers/sample_trigger.ts
+++ triggers/sample_trigger.ts
@@ -1,4 +1,4 @@
-import { Trigger } from "deno-slack-sdk/types.ts";
+import type { Trigger } from "deno-slack-sdk/types.ts";
 import { TriggerContextData, TriggerTypes } from "deno-slack-api/mod.ts";
 import SampleWorkflow from "../workflows/sample_workflow.ts";
 /**
